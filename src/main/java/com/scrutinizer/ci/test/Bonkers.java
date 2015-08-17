96cb5a23843cdddd8d660b41cbb1560883849a58
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-17 18:27:10 +0200

    gave Bonkers some mazing contents

--- src/main/java/com/scrutinizer/ci/test/Bonkers.java
+++ src/main/java/com/scrutinizer/ci/test/Bonkers.java
@@ -1,3 +1,14 @@
 public class Bonkers {
-
+private int mazing(int x, int y, int z) {
+  if (x>0) {
+    if (y > 0) {
+      if (z>0) {
+        return x+y+z;
+      }
+    return y;
+    }
+    return x;
+  }
+  return 0;
+}
 }
