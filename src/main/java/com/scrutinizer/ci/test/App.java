6161891d40d7d752185975f1200457871f6df821
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-17 18:23:15 +0200

    added dupe functions

--- src/main/java/com/scrutinizer/ci/test/App.java
+++ src/main/java/com/scrutinizer/ci/test/App.java
@@ -12,4 +12,30 @@ public class App
         //TODO: auch duh!
         System.out.println( "Hello World!" );
     }
+    
+    private String dupe1() {
+        while(true) {
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            System.out.println("4");
+            System.out.println("5");
+            System.out.println("6");
+            System.out.println("7");
+            break;
+        }
+    }
+    
+     private String dupe2() {
+        while(true) {
+            System.out.println("1");
+            System.out.println("2");
+            System.out.println("3");
+            System.out.println("4");
+            System.out.println("5");
+            System.out.println("6");
+            System.out.println("7");
+            break;
+        }
+    }
 }
