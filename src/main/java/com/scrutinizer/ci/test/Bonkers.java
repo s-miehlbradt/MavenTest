7dddf168355069ef1323cbce5557508e35205cb5
Sabine Miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-24 09:13:12 +0200

    added a dupe method

--- src/main/java/com/scrutinizer/ci/test/Bonkers.java
+++ src/main/java/com/scrutinizer/ci/test/Bonkers.java
@@ -1,6 +1,8 @@
 package com.scrutinizer.ci.test;
 
 public class Bonkers {
+  //FIXME duh!
+  //TODO: auch duh!
 public int mazing(int x, int y, int z) {
   if (x>0) {
     if (y > 0) {
@@ -13,4 +15,18 @@ public int mazing(int x, int y, int z) {
   }
   return 0;
 }
+
+  public int amazing(int x, int y, int z) {
+    if (x>0) {
+      if (y > 0) {
+        if (z>0) {
+          return x+y+z;
+        }
+        return y;
+      }
+      return x;
+    }
+    return 0;
+  }
 }
+
