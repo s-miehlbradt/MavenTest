9931e5a0eeae5171b57042f651374aecea8f669b
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-26 17:31:38 +0200

    Update App.java

--- src/main/java/com/scrutinizer/ci/test/App.java
+++ src/main/java/com/scrutinizer/ci/test/App.java
@@ -6,6 +6,14 @@ package com.scrutinizer.ci.test;
  */
 public class App 
 {
+ public String val1 = "CONSTANT";
+ public String val2 = "CONSTANT";
+ public String val3 = "CONSTANT";
+ 
+ public String longval1 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ public String longval2 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ public String longval3 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ 
     public static void main( String[] args )
     {
         //FIXME duh!
