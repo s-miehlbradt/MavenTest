afaf2417c9f68b0efcc4f0b98a132f8eb902ebcd
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-26 17:27:24 +0200

    Update S1192.java

--- src/main/java/com/scrutinizer/ci/test/S1192.java
+++ src/main/java/com/scrutinizer/ci/test/S1192.java
@@ -1,4 +1,5 @@
 public class S1192 {
+ private void doNothing() {
  String val1 = "CONSTANT";
  String val2 = "CONSTANT";
  String val3 = "CONSTANT";
@@ -6,4 +7,5 @@ public class S1192 {
  String longval1 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
  String longval2 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
  String longval3 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ }
 }
