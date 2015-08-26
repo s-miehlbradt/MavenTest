e7f5dd0d417e44bb3fe56f0f2243631fd4c1f8d9
s-miehlbradt <sabine.miehlbradt@scrutinizer-ci.com>
2015-08-26 17:17:47 +0200

    Create S1192.java

---
+++ src/main/java/com/scrutinizer/ci/test/S1192.java
@@ -0,0 +1,10 @@
+public class S1192 {
+ void doSomething()
+ String val1 = "CONSTANT";
+ String val2 = "CONSTANT";
+ String val3 = "CONSTANT";
+ 
+ String longval1 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ String longval2 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+ String longval3 = "ABCDEFGHIJKLMNOPQRSTUVXYZ";
+}
